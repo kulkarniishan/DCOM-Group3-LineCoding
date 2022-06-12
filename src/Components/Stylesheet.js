d79386a80ada9f3e587cddd616753b88468bff7f
parthk2510 <87540263+parthk2510@users.noreply.github.com>
2022-06-12 15:21:01 +0530

    Create Stylesheet.js

---
+++ src/Components/Stylesheet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Styles.css'
+function Stylesheet(){
+    return (
+        <div>
+            <h1 className='primary'>Line </h1>
+            <a>My name is parth </a>
+        </div>
+    )
+}
+
+export default Stylesheet
