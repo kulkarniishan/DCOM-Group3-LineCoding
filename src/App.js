9fcf530e39614393d8b3a2ff091e05873c10e9d7
parthk2510 <87540263+parthk2510@users.noreply.github.com>
2022-06-13 12:22:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div>
        <div>
+        
       <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
   <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 postion-static" href="/">Line encoding Techinque</a>
   <div className="navbar-nav">
